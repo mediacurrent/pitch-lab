6aeadba5fc3addecb5fb03fe4be989b50b8399ca
Elliott Mower <ecmower@gmail.com>
2025-12-08 15:12:24 -0500

    Update Next.js to 16.0.7 and related dependencies
    - Updated Next.js from 14.1.0 to 16.0.7
    - Updated eslint-config-next to 16.0.6
    - Updated ESLint from 8 to 9
    - Updated next-sanity from 9.12.3 to 11.6.10
    - Updated Sanity from 3.99.0 to 4.19.0
    - Fixed defineLive import path for next-sanity v11
    - Removed deprecated images.domains from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com', 'cdn.sanity.io'],
     remotePatterns: [
       {
         protocol: 'https',
